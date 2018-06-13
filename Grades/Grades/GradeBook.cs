68b17057cafe971441a4e46e9e0f4faa92b5be92
Unknown <cameronherwig@gmail.com>
2018-06-12 21:38:25 -0400

    Adding To GitHub

---
+++ Grades/Grades/GradeBook.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    public class GradeBook : GradeTracker
+    {
+        public GradeBook()
+        {
+            _name = "Empty";
+            grades = new List<float>();
+        }
+
+        public override void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        public override void WriteGrades(TextWriter destination)
+        {
+            foreach (int grade in grades)
+            {
+                destination.WriteLine(grade);
+            }
+        }
+
+        public override GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics stats = new GradeStatistics();
+            float sum = 0;
+            foreach (float grade in grades)
+            {
+                sum += grade;
+            }
+            stats.AverageGrade = sum / grades.Count();
+            stats.HighestGrade = grades.Max();
+            stats.LowestGrade = grades.Min();
+            return stats;
+        }
+        
+
+        public override IEnumerator GetEnumerator()
+        {
+            return grades.GetEnumerator();
+        }
+
+        protected List<float> grades;
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-C-Sharp
+Hello - A basic Hello World program.
+
+Grades - A gradebook programming which showcases interfaces and other OOP practices, flow control, exception handling, events, and unit testing. 
