1d58491dd16bf624cb6da41b89cb40c1c215ed85
Unknown <cameronherwig@gmail.com>
2018-06-18 13:18:23 -0400

    Adding Base Project + Delegates

---
+++ DelegatesAndEvents/DelegatesAndEvents/Worker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesAndEvents
+{
+    public delegate int WorkPerformedHandler(int hours, WorkType workType);
+
+    public class Worker
+    {
+        public event WorkPerformedHandler WorkPerformed;
+        public event EventHandler WorkCompleted;
+
+        public void DoWork(int hours, WorkType workType)
+        {
+            for (int i = 0; i < hours; i++)
+            {
+                
+            }
+        }
+
+        protected virtual void OnWorkPerformed(int hours, WorkType workType)
+        {
+
+        }
+    }
+}
