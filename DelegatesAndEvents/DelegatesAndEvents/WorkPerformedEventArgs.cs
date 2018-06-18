f99636eb59f449cca1c4fef4ad24296afbe25335
Unknown <cameronherwig@gmail.com>
2018-06-18 13:35:37 -0400

    Added Events

--- DelegatesAndEvents/DelegatesAndEvents/WorkPerformedEventArgs.cs
+++ DelegatesAndEvents/DelegatesAndEvents/WorkPerformedEventArgs.cs
@@ -8,6 +8,12 @@ namespace DelegatesAndEvents
 {
     public class WorkPerformedEventArgs : EventArgs
     {
-
+        public WorkPerformedEventArgs(int hours, WorkType workType)
+        {
+            Hours = hours;
+            WorkType = workType;
+        }
+        public int Hours { get; set; }
+        public WorkType WorkType { get; set; }
     }
 }
--- DelegatesAndEvents/DelegatesAndEvents/Worker.cs
+++ DelegatesAndEvents/DelegatesAndEvents/Worker.cs
@@ -6,24 +6,38 @@ using System.Threading.Tasks;
 
 namespace DelegatesAndEvents
 {
-    public delegate int WorkPerformedHandler(int hours, WorkType workType);
+    //public delegate int WorkPerformedHandler(object sender, WorkPerformedEventArgs e);
 
     public class Worker
     {
-        public event WorkPerformedHandler WorkPerformed;
+        public event EventHandler<WorkPerformedEventArgs> WorkPerformed;
         public event EventHandler WorkCompleted;
 
         public void DoWork(int hours, WorkType workType)
         {
             for (int i = 0; i < hours; i++)
             {
-                
+                OnWorkPerformed(i + 1, workType);
+            }
+            OnWorkCompleted();
+        }
+    
+        protected virtual void OnWorkCompleted()
+        {
+            var del = WorkCompleted as EventHandler;
+            if (del != null)
+            {
+                del(this, EventArgs.Empty);
             }
         }
 
         protected virtual void OnWorkPerformed(int hours, WorkType workType)
         {
-
+            var del = WorkPerformed as EventHandler<WorkPerformedEventArgs>;
+            if(del != null)
+            {
+                del(this, new WorkPerformedEventArgs(hours, workType));
+            }
         }
     }
 }
