2d3a3c1f3ba602e712564124844c5a69dafaff9f
Unknown <cameronherwig@gmail.com>
2018-06-18 15:32:32 -0400

    Added Lambdas and Different Delegates

--- DelegatesAndEvents/DelegatesAndEvents/Program.cs
+++ DelegatesAndEvents/DelegatesAndEvents/Program.cs
@@ -6,30 +6,54 @@ using System.Threading.Tasks;
 
 namespace DelegatesAndEvents
 {
-    //public delegate void WorkPerformedHandler(object sender, WorkPerformedEventArgs e);
+    public delegate int BizRulesDelegate(int x, int y);
 
     class Program
     {
         static void Main(string[] args)
         {
-            //WorkPerformedHandler del1 = new WorkPerformedHandler(WorkPerformed1);
-            //WorkPerformedHandler del2 = new WorkPerformedHandler(WorkPerformed2);
-            //WorkPerformedHandler del3 = new WorkPerformedHandler(WorkPerformed3);
 
-            //del1 += del2 + del3;
+            var custs = new List<Customer>
+            {
+                new Customer { City = "Phoenix", FirstName = "John", LastName = "Doe", ID = 1},
+                new Customer { City = "Phoenix", FirstName = "Jane", LastName = "Doe", ID = 500},
+                new Customer { City = "Seattle", FirstName = "Suki", LastName = "Pizzoro", ID = 3},
+                new Customer { City = "New York City", FirstName = "Michelle", LastName = "Smith", ID = 4},
+            };
 
-            //int finalHours = del1(10, WorkType.GenerateReports);
-            //Console.WriteLine(finalHours);
+            var phxCusts = custs.Where(c => c.City == "Phoenix" && c.ID < 500)
+                .OrderBy(c => c.FirstName);
+            foreach (var cust in phxCusts)
+            {
+                Console.WriteLine(cust.FirstName);
+            }
+
+            var data = new ProcessData();
+            BizRulesDelegate addDel = (x, y) => x + y;
+            BizRulesDelegate multiplyDel = (x, y) => x * y;
+            data.Process(2, 3, addDel);
+            data.Process(2, 3, multiplyDel);
+
+            Func<int, int, int> funcAddDel = (x, y) => x + y;
+            Func<int, int, int> funcMultiplyDel = (x, y) => x * y;
+            data.ProcessFunc(3, 2, funcAddDel);
+            data.ProcessFunc(3, 2, funcMultiplyDel);
+
+            Action<int, int> myAction = (x, y) => Console.WriteLine(x + y);
+            Action<int, int> myMultiplyAction = (x, y) => Console.WriteLine(x * y);
+            data.ProcessAction(2, 3, myAction);
+            data.ProcessAction(2, 3, myMultiplyAction);
+
 
             var worker = new Worker();
-            worker.WorkPerformed += delegate (object sender, WorkPerformedEventArgs e)
+
+            worker.WorkPerformed += (s, e) =>
             {
                 Console.WriteLine("Hours Worked: " + e.Hours + " " + e.WorkType);
+                //Console.WriteLine("Something else.");
             };
-            worker.WorkCompleted += delegate (object sender, EventArgs e)
-            {
-                Console.WriteLine("Worker is done");
-            };
+            worker.WorkCompleted += (s,e) => Console.WriteLine("Worker is done"); 
+
             worker.DoWork(8,WorkType.GenerateReports);       
             Console.Read();
         }
