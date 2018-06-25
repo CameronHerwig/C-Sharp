f777a1ba0a8348c77dd93985dbd80b904fed96d9
Unknown <cameronherwig@gmail.com>
2018-06-25 14:37:30 -0400

    Validating Parameters

--- ACM - DefensiveCoding/ACM.BL/Customer.cs
+++ ACM - DefensiveCoding/ACM.BL/Customer.cs
@@ -25,5 +25,25 @@ namespace ACM.BL
             // If not,
             // request an email address from the user.
         }
+
+        public decimal CalculatePercentOfGoalSteps(string goalStepCount, string actualStepCount)
+        {
+            decimal goalSteps = 0;
+            decimal actualSteps = 0;
+
+            if (string.IsNullOrWhiteSpace(goalStepCount)) throw new ArgumentException("Goal must be entered", "goalSteps");
+            if (string.IsNullOrWhiteSpace(actualStepCount)) throw new ArgumentException("Actual steps count must be entered", "actualSteps");
+
+            if (!decimal.TryParse(goalStepCount, out goalSteps)) throw new ArgumentException("Goal must be numeric", "goalSteps");
+            if (!decimal.TryParse(actualStepCount, out actualSteps)) throw new ArgumentException("Actual steps must be numeric", "actualSteps");
+
+            return CalculatePercentOfGoalSteps(goalStepCount, actualStepCount);
+        }
+
+        public decimal CalculatePercentOfGoalSteps(decimal goalStepCount, decimal actualStepCount)
+        {
+            if (goalStepCount <= 0) throw new ArgumentException("Goal must be greater than 0", "goalSteps");
+            return (actualStepCount / goalStepCount) * 100;
+        }
     }
 }
